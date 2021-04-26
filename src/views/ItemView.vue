e8221f4ec2e9d8b189c7e72469d7bc87b0da7032
SeungHwan Jeon <43638373+MonkCodingg@users.noreply.github.com>
2021-04-26 10:24:11 +0900

    이벤트 버스를 이용한 스피터 컴포넌트 구현

--- src/views/ItemView.vue
+++ src/views/ItemView.vue
@@ -2,19 +2,14 @@
   <div>
     <section>
       <!-- 사용자 정보 -->
-      <!-- <div class="user-container">
-        <div>
-          <i class="fas fa-user"></i>
-        </div>
-        <div class="user-description">
-          <router-link :to="`/user/${fetchedItem.user}`">
-            {{ fetchedItem.user }}
-          </router-link>
-          <div class="time">
-            {{ fetchedItem.time_ago }}
-          </div>
-        </div>
-      </div> -->
+      <user-profile :info="fetchedItem">
+        <!-- <div slot="username">{{ fetchedItem.user }}</div> -->
+        <router-link slot="username" :to="`/user/${fetchedItem.user}`">
+          {{ fetchedItem.user }}
+        </router-link>
+        <template slot="time">{{ 'Posted ' + fetchedItem.time_ago }}</template>
+      </user-profile>
+
     </section>
     <section>
       <h2>{{ fetchedItem.title }}</h2>
@@ -28,8 +23,12 @@
 </template>
 
 <script>
+import UserProfile from '../components/UserProfile.vue';
 import {mapGetters} from 'vuex';
 export default {
+  components: {
+    UserProfile
+  },
   computed: {
     ...mapGetters(['fetchedItem']),
   },
