96131aed8e05554a4ffac03e4ba28d9d17dec04f
MonkCodingg <laevus.j@gmail.com>
2021-06-02 19:41:22 +0900

    async 함수 이용한 코드 리팩토링

--- src/api/index.js
+++ src/api/index.js
@@ -10,16 +10,25 @@ function fetchNewsList() {
     // return axios.get(config.baseUrl + 'news/1.json');
     return axios.get(`${config.baseUrl}news/1.json`);
 }
-function fetchAskList() {
-    // return axios.get(config.baseUrl + 'ask/1.json');
-    return axios.get(`${config.baseUrl}ask/1.json`);
+async function fetchAskList() {
+    try {
+        const response = await axios.get(`${config.baseUrl}ask/1.json`);
+        return response;
+    } catch (error) {
+        console.log(error);   
+    }
 }
 function fetchJobsList() {
     // return axios.get(config.baseUrl + 'jobs/1.json')
     return axios.get(`${config.baseUrl}jobs/1.json`);
 }
-function fetchList(pageName) {
-  return axios.get(`${config.baseUrl}${pageName}/1.json`);
+async function fetchList(pageName) {
+    try {
+        const response = await axios.get(`${config.baseUrl}${pageName}/1.json`);
+        return response;
+    } catch (error) {
+        console.log(error);
+    }    
 }
 function fetchUserInfo(username) {
     return axios.get(`${config.baseUrl}user/${username}.json`);
@@ -36,6 +45,3 @@ export {
     fetchUserInfo,
     fetchCommentItem,
 }
-
-
- 
--- src/store/actions.js
+++ src/store/actions.js
@@ -7,63 +7,34 @@ import {
     fetchCommentItem, 
         } from '../api/index.js';
 export default {
-    FETCH_NEWS(context) {
-        return fetchNewsList()
-            .then(response => {
-                //console.log(response.data);
-                context.commit('SET_NEWS', response.data);
-                //state.news = response.data;
-                return response;
-            }) 
-            .catch(error => {
-                console.log(error);
-            });
+    async FETCH_NEWS(context) {
+        const response = await fetchNewsList();
+        context.commit('SET_NEWS', response.data);
+        return response;    
     },
-    FETCH_ASK({ commit }) {
-        return fetchAskList()
-            .then(({ data }) => {
-                //console.log(data);
-                commit('SET_ASK', data);
-            })
-            .catch(error => {
-                console.log(error);
-            });
+    async FETCH_ASK({ commit }) {
+        const response = await fetchAskList()
+        commit('SET_ASK', response.data);
+        return response;
     },
-    FETCH_JOBS({ commit }) {
-        return fetchJobsList()
-            .then(({ data }) => {
-                //console.log(data);
-                commit('SET_JOBS', data);
-            })
-            .catch(error => {
-                console.log(error);
-            })
+    async FETCH_JOBS({ commit }) {
+        const response = await fetchJobsList()
+        commit('SET_JOBS', response.data);
+        return response;
     },
-    FETCH_USER({ commit }, name){
-       return fetchUserInfo(name)
-            .then(({ data }) => {
-                commit('SET_USER', data);
-            })
-            .catch(error => {
-                console.log(error);
-            });
+    async FETCH_USER({ commit }, name){
+        const response = await fetchUserInfo(name);
+        commit('SET_USER', response.data);
+        return response;
+     },
+    async FETCH_ITEM({ commit }, id) {
+        const response = await fetchCommentItem(id);
+        commit('SET_ITEM', response.data);
+        return response;            
     },
-    FETCH_ITEM({ commit }, id) {
-        return fetchCommentItem(id)
-            .then(({ data }) => {
-                commit('SET_ITEM', data);
-            })
-            .catch(error => {
-                console.log(error);
-            });
-    },
-    FETCH_LIST({ commit }, pageName) {
-      return fetchList(pageName)
-        .then(response => {
-          commit('SET_LIST', response.data);
-          return response;
-        })
-        .catch(error => console.log(error));
+    async FETCH_LIST({ commit }, pageName) {
+        const response = await fetchList(pageName);
+        commit('SET_LIST', response.data);
+        return response;
     }
-
 }
